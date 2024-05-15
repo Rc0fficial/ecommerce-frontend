75d03763869b1ab4f192c7f5b0e2bb446332b75a
rc0fficial <to.arsalanali@gmail.com>
2024-05-15 17:26:01 +0500

    update on screen

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import Image from "next/image";
 import { ArrowDown } from "../svg/ArrowDown.svg";
 import { Search } from "../svg/Search.svg";
+import { Cart } from "../svg/Cart.svg";
+import { Hand } from "../svg/Hand.svg";
 
 const navbar = () => {
   return (
-    <div className="flex py-[16px] items-center justify-between">
+    <div className="px-4 flex py-[16px] items-center justify-between">
       <div>
         <Image width={80} height={47} src="/images/logo.png" />
       </div>
-      <div>
+      <div className="hidden md:block">
         <ul className="flex ">
           <li className="text-[#393A44] text-[14px] px-[24px] border-r cursor-pointer">
             home
@@ -22,7 +24,7 @@ const navbar = () => {
           </li>
         </ul>
       </div>
-      <div>
+      <div className="hidden md:block">
         <div className="relative">
           <input
             type="text"
@@ -32,6 +34,32 @@ const navbar = () => {
 
           <div className="absolute left-[9px] top-[5px] h-5 w-5 text-gray-500">
             <Search />
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center justify-between">
+        <div>
+          <ul className="flex ">
+            <li className="text-[#393A44] text-[14px] px-[24px] flex gap-[8px] cursor-pointer">
+              <Cart />
+              <span className="hidden md:block">cart</span>
+            </li>
+            <li className="text-[#393A44] text-[14px] px-[24px] border-l flex gap-[8px] cursor-pointer">
+              <Hand />
+              <span className="hidden md:block">Become Seller</span>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <div className="relative">
+            <div className="w-10 h-10 overflow-hidden bg-gray-100 rounded-full cursor-pointer">
+              <Image
+                alt="ventor"
+                width={80}
+                height={47}
+                src="/images/vector.png"
+              />
+            </div>
           </div>
         </div>
       </div>
