777f2bd4ef4e599b41ec0c40c7452a09e4745b03
rc0fficial <to.arsalanali@gmail.com>
2024-05-15 20:35:37 +0500

    update on navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,12 +4,19 @@ import { ArrowDown } from "../svg/ArrowDown.svg";
 import { Search } from "../svg/Search.svg";
 import { Cart } from "../svg/Cart.svg";
 import { Hand } from "../svg/Hand.svg";
+import { Hamburger } from "../svg/Hamburger.svg";
 
 const navbar = () => {
   return (
-    <div className="px-4 flex py-[16px] items-center justify-between">
-      <div>
-        <Image width={80} height={47} src="/images/logo.png" />
+    <div className="px-4">
+    <div className=" flex py-[16px] items-center justify-between">
+      <div className="flex items-center justify-between gap-2">
+        <div className="md:hidden cursor-pointer">
+          <Hamburger />
+        </div>
+        <div>
+          <Image alt="logo" width={80} height={47} src="/images/logo.png" />
+        </div>
       </div>
       <div className="hidden md:block">
         <ul className="flex ">
@@ -39,18 +46,18 @@ const navbar = () => {
       </div>
       <div className="flex items-center justify-between">
         <div>
-          <ul className="flex ">
-            <li className="text-[#393A44] text-[14px] px-[24px] flex gap-[8px] cursor-pointer">
+          <ul className="flex items-center gap-[14px] md:gap-0">
+            <li className="text-[#393A44] text-[14px]  md:px-[24px] flex gap-[8px] cursor-pointer">
               <Cart />
               <span className="hidden md:block">cart</span>
             </li>
-            <li className="text-[#393A44] text-[14px] px-[24px] border-l flex gap-[8px] cursor-pointer">
+            <li className="text-[#393A44] text-[14px]  md:px-[24px] md:border-l flex gap-[8px] cursor-pointer">
               <Hand />
               <span className="hidden md:block">Become Seller</span>
             </li>
           </ul>
         </div>
-        <div>
+        <div className="ml-[14px] md:ml-0">
           <div className="relative">
             <div className="w-10 h-10 overflow-hidden bg-gray-100 rounded-full cursor-pointer">
               <Image
@@ -64,6 +71,20 @@ const navbar = () => {
         </div>
       </div>
     </div>
+     <div className=" md:hidden">
+     <div className="relative">
+       <input
+         type="text"
+         placeholder="Search"
+         className="w-full  border-gray-300   border-[1px] rounded-full text-[#909198] text-[14px] pl-12 pr-4 py-2 focus:outline-none focus:border-[#909198]"
+       />
+
+       <div className="absolute left-[9px] top-[5px] h-5 w-5 text-gray-500">
+         <Search />
+       </div>
+     </div>
+   </div>
+   </div>
   );
 };
 
